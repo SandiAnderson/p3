2139533bbc1e421dc583ea6d26690be0bda6d2de
Sandra Anderson <anderson.sandi@gmail.com>
2018-10-16 17:13:45 -0700

    added initial routes

---
+++ app/Http/Controllers/RaceTimeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RaceTimeController extends Controller
+{
+    //
+    public function welcome()
+    {
+        return 'search screen here';
+    }
+
+    public function calcRace()
+    {
+        return 'racetime calculation';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'RaceTimeController@welcome');
+
+Route::any('/calc', 'RaceTimeController@calcRace');
