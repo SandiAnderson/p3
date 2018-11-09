4b93bd1773fd39e441c161ff88fda63f1cb8c24e
Sandra Anderson <anderson.sandi@gmail.com>
2018-11-08 18:06:28 -0800

    fix validation

--- resources/views/modules/displayvalue.blade.php
+++ resources/views/modules/displayvalue.blade.php
@@ -1,7 +1,7 @@
 @if($type=='number')
     value='{{old($source) ?? (isset($source)? session($source):'0') }}'
 @elseif($type=='text')
-    value='{{old($source,'DD/MM/YYYY')}}'
+    value='{{old($source,'MM/DD/YYYY')}}'
 @elseif($type=='select')
     {{ old($source, session($source)) == $value ? 'selected' : '' }}
 @elseif($type=='radio')
