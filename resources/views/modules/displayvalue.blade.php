260dab8d2d24dec318d2bcae6c600fff108c92a5
Sandra Anderson <anderson.sandi@gmail.com>
2018-11-08 18:05:45 -0800

    fix validation

--- resources/views/modules/displayvalue.blade.php
+++ resources/views/modules/displayvalue.blade.php
@@ -1,7 +1,7 @@
 @if($type=='number')
     value='{{old($source) ?? (isset($source)? session($source):'0') }}'
 @elseif($type=='text')
-    value='{{old($source,'YYYY/MM/DD')}}'
+    value='{{old($source,'DD/MM/YYYY')}}'
 @elseif($type=='select')
     {{ old($source, session($source)) == $value ? 'selected' : '' }}
 @elseif($type=='radio')
